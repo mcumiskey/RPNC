68292503d11e2515d54c9273cddf5d053993f553
Miles <milescumiskey@gmail.com>
2018-11-30 07:58:31 -0500

    Using LinkedList Stack

--- RPNC.cpp
+++ RPNC.cpp
@@ -10,8 +10,94 @@ Individual expressions in RPN format (see below) are read from the file.
 If an expression is well-formed the program will evaluate it and display the expression along with its results on the console. 
 Malformed expressions result in an error message being displayed on the console.
    
-Input: an ASCII text files with the names of the files to process
-Output: Wordcount and linecount of included files. Allows for searching documents for specific words. Displays the result
+Input: an ASCII text files with formatted numbers and operands - can also be entered via command line
+Output: Math
 Preconditions: none
 Postconditions: none
 ---------------------------*/
+
+#include <string> 
+#include <iostream>
+
+using namespace std;
+
+//Linked list is the underlying data structure for my stack
+class Node {
+    public:
+        int data;
+        Node *next;
+};
+
+class LinkedList {
+    private: 
+        Node *head;
+
+    public:
+        LinkedList() {
+            head = NULL;
+        }
+
+        bool empty() {
+            return (head == NULL);
+        }
+        void push(int value) {
+            Node *temp = new Node();
+            temp->data = value;
+            temp->next = head;
+            head = temp;
+        }
+        int pop(){
+            int temp = head->data;
+            Node *oldHead = head;
+            head = head->next;
+            delete oldHead;
+            return temp;
+        }
+        int peek() {
+            if(!empty()){
+                return head->data;
+            } 
+        }
+};
+
+//Basic Stack
+class Stack {
+    private: 
+     LinkedList data;
+     int s = 0;
+
+     public:
+
+     bool empty() {
+         return data.empty();
+     }
+
+     void push(int value) {
+         cout << "pushing " << value << endl;
+         data.push(value);
+         s++;
+     }
+
+     int pop(){
+         if (data.empty())    
+         {
+            cout << "Error on pop: Stack is empty" << endl;
+            return -99;
+         } else {
+            s--;
+            return data.pop();
+         }       
+     }
+
+     int peek() {
+         if (empty()) {
+            cout << "Error on peek: Stack is empty" << endl;
+            return -99;
+         } else {
+            return data.peek();
+         }
+     }
+};
+
+int main(int argc, char* argv[]){
+}
